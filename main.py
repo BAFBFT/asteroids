013dcdee07b615656e8ef34c818ea733b1bccfaa
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2025-06-27 13:45:31 +0100

    added screen to game

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ from constants import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!\n")
-    print(f"Screen width: {SCREEN_WIDTH}\n")
-    print(f"Screen height: {SCREEN_HEIGHT}\n")
+
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
